95bfe2328349f95b5129031673da287d057ae9fa
theblessedgarden <theblessedgarden@proton.me>
2025-03-03 20:36:44 +0500

    Add publishing plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("java")
+    id("maven-publish")
 }
 
 group = "net.colorfulmc"
@@ -12,6 +13,14 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            from(components.findByName("java"))
+        }
+    }
 }
 
 tasks.test {
